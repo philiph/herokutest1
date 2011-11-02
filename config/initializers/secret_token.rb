0d07956ecf044ebda13c6f7356d6c739e83c1ae7
Philip Hagen <phil@Athena.local>
2011-11-03 00:10:51 +0800

    Initial commit.  New Rails 3.1.1 app.  Controller and index action. PostgreSQL DB.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herokutest1::Application.config.secret_token = '04467b0410140f71308034a5cfd8698eaad1dc8dba5f0498d7549acd2a3511cde6fb5ae158c14c15e7e85c3218261e3f5a364d16cf49f590385a2e9260d88047'
